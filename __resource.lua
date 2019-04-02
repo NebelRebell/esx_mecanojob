8bf5ab9a42b58b56d50106179f89da0cccbdbdd5
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-04-02 03:40:36 +0200

    Insert dependencies
    dependencies {
    	'es_extended',
    	'esx_billing'
    }

--- __resource.lua
+++ __resource.lua
@@ -2,28 +2,23 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Mechanic Job'
 
-version '1.1.0'
+version '1.1.1'
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/br.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
+	'locales/de.lua',
 	'config.lua',
 	'client/main.lua'
 }
 
 server_scripts {
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/br.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
+	'locales/de.lua',
 	'config.lua',
 	'server/main.lua'
+}
+
+dependencies {
+	'es_extended',
+	'esx_billing'
 }
